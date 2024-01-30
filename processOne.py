af900f84ced31b6434ebf342b0b22197cd2ff26e
theSoberSobber <akshatshrivastava0010@gmail.com>
2024-01-30 15:40:36 +0530

    Fixed Hash file race condition

--- processOne.py
+++ processOne.py
@@ -1,11 +1,14 @@
 import time, os, json, sys
 
 # from hashUtil import get_file_hash
-from data import collegeIds, hash_file_path
+from data import collegeIds
 from fresourcesUtil import FresourcesUtil
 from uploader import CatboxUploader
 from processor import DataProcessor
 from hashUtil import HashUtil
+
+file_path = "data-" + sys.argv[1] + ".json"
+hash_file_path = "hashmap-" + sys.argv[1] + ".json"
 
 # Example usage:
 api_handler = FresourcesUtil()  # Replace with your actual instantiation code
@@ -16,11 +19,10 @@ data_processor = DataProcessor(api_handler, upload_handler, hash_util)
 oneDict = {}
 oneDict[sys.argv[1]] = collegeIds[sys.argv[1]];
 
-file_path = sys.argv[1] + ".json"
-
 data = data_processor.process_all_colleges(oneDict)
 
 with open(file_path, "w") as json_file:
     json.dump(data, json_file, indent=4)
 
 print(f"JSON data has been written to '{file_path}'")
+print(f"JSOn hash data has been written to '{hash_file_path}'")
